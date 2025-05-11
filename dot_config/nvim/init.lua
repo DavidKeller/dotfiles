76d28ffb8ccc699963571891ee45c207f698501b
David Keller <david.keller@litchis.fr>
2025-05-11 11:52:59 +0200

    Initial commit
    Signed-off-by: David Keller <david.keller@litchis.fr>

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,42 @@
+-- Color
+vim.cmd.colorscheme('habamax')
+
+-- Ruler
+vim.opt.ruler = true
+
+-- Scroll
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.wrap = false
+
+-- Highlight
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Files
+vim.opt.swapfile = false
+
+vim.opt.colorcolumn = '80'
+
+-- Tab
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+
+-- Languages
+vim.opt.makeprg = 'ninja'
+vim.diagnostic.config({ virtual_text = true })
+vim.lsp.config['clangd'] = {
+  cmd = {
+    'clangd',
+    '--clang-tidy',
+    '--enable-config',
+    '--background-index',
+    '--offset-encoding=utf-8',
+  },
+  root_markers = { 'compile_commands.json' },
+  filetypes = { 'c', 'cpp' },
+}
+vim.lsp.enable('clangd')
